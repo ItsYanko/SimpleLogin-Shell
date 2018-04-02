ae30a76ea0ad9952784a112298eb84aa4e20e77c
Ian Paulo Perina <36312973+Ian-PP@users.noreply.github.com>
2018-04-02 23:53:15 +0000

    Now uses extarnal files (other Programs)

--- Main.sh
+++ Main.sh
@@ -35,9 +35,9 @@ if [ "$insert_usr" = "$username && "$insert_pswd" = "$password" ]
 then
 echo "--Username and Password Correct!"
 echo "--Redirecting to Dashboard"
-sh ./files/dashboard.sh
+sh ./files/dashboard.sh               #The Username and the Password are correct, redirecting user to the Dashboard          
 else
-echo "--Username or Password incorrect, exiting"
+echo "--Username or Password incorrect, exiting"    #The Username or the Password is incorrect, takes the user out of the Program
 fi 
 
 $SHELL        #To keep the terminal open
